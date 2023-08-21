f047522e4a8c96c7067bb3d99c2f25d5392e461e
[zhangbiaobin] <[zhangbiaobin@leyaoyao.com]>
2023-08-21 09:57:41 +0800

    add：测试

--- src/main.js
+++ src/main.js
@@ -1,50 +1,43 @@
 import Vue from 'vue'
-import { preFetchLib, bindVueRuntime } from 'hel-micro'
 
-bindVueRuntime({ Vue })
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
-// const fetchOptions = {
-//   async getSubAppAndItsVersionFn() {
-//     const res = await fetch(
-//       "https://hel-eco.github.io/hel-tpl-remote-vue-comp/as_v1/hel-meta.json"
-//     );
-//     const meta = await res.json();
-//     console.log("meta", meta);
-//     return meta;
-//   }
-// };
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-// 本地联调
-const enableCustom = !!window.location.port
-const fetchOptions = {
-  custom: {
-    host: 'http://localhost:7001',
-    enable: enableCustom,
-    /**
-     * defaut: false
-     * 是否跳过获取 hel-meta.json 的获取步骤，true：跳过，false：不跳过
-     * 当用户设定 custom.host 配置时，hel-micro 采取总是相信该 host 存在一个 hel-meta.json 文件并尝试去获取
-     * 如获取失败时再去解析该 host 对应的首页并现场解析出 hel-meta.json 数据
-     * 因此获取动作可能会报一个 404 not found 符合预期的行为，用户可设定 skipFetchHelMeta 为 true 跳过此步骤
-     * 但建议加载线上模块时（非本地联调时），保持 skipFetchHelMeta 为 false 比较好，有利于提高模块加载速度（ 无html解析hel-meta.json过程 ）
-     */
-    skipFetchHelMeta: true
-  }
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+// import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
 }
 
-async function main() {
-  // console.log('fetchOptions:', fetchOptions)
-  // from unpkg by default
-  // see https://unpkg.com/hel-tpl-remote-vue-comps@1.1.3/hel_dist/index.html
-  // await preFetchLib("hel-tpl-remote-vue-comps");
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
 
-  // from user custom deploy location
-  // see https://hel-eco.github.io/hel-tpl-remote-vue-comp/index.html
-  await preFetchLib('lib-zhangbb-component', fetchOptions)
+Vue.config.productionTip = false
 
-  // await preFetchLib('lib-zhangbb')
-
-  await import('./loadApp')
-}
-
-main().catch(console.error)
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,54 @@
 <template>
-  <div>home</div>
+  <div class="hello">
+    <RemoteCompAsyncBetter name="loaded in codesandbox" />
+  </div>
 </template>
+
+<script>
+// https://github.com/hel-eco/hel-tpl-remote-vue-comp
+import { preFetchLib, core } from "hel-micro";
+
+// 本地联调
+const enableCustom = !!window.location.port
+const fetchOptions = {
+  custom: {
+    host: 'http://localhost:7001',
+    enable: enableCustom,
+    /**
+     * defaut: false
+     * 是否跳过获取 hel-meta.json 的获取步骤，true：跳过，false：不跳过
+     * 当用户设定 custom.host 配置时，hel-micro 采取总是相信该 host 存在一个 hel-meta.json 文件并尝试去获取
+     * 如获取失败时再去解析该 host 对应的首页并现场解析出 hel-meta.json 数据
+     * 因此获取动作可能会报一个 404 not found 符合预期的行为，用户可设定 skipFetchHelMeta 为 true 跳过此步骤
+     * 但建议加载线上模块时（非本地联调时），保持 skipFetchHelMeta 为 false 比较好，有利于提高模块加载速度（ 无html解析hel-meta.json过程 ）
+     */
+    skipFetchHelMeta: true
+  }
+}
+export default {
+  name: "HelloWorld",
+  components: {
+    // RemoteCompAsyncOld: async () => {
+    //   const name = "hel-tpl-remote-vue-comps";
+    //   const mod = await preFetchLib(name);
+    //   // const mod = await preFetchLib("remote-selection-point", "0.0.1-alpha.3");
+    //   // const mod = await preFetchLib("hel-tpl-remote-vue-comps");
+    //   return mod.App;
+    // },
+
+    // better way, see doc: https://v2.cn.vuejs.org/v2/guide/components-dynamic-async.html
+    RemoteCompAsyncBetter: () => ({
+      component: new Promise(async (r, j) => {
+        try {
+          const mod = await preFetchLib('lib-zhangbb-component', fetchOptions)
+          console.log("mod===>",mod)
+          r(mod.SomeModule
+);
+        } catch (err) {
+          j(err);
+        }
+      }),
+    }),
+  },
+};
+</script>
